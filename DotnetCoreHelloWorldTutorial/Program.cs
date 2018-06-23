d97270649833c0d886defaa7e4db2d30812a8e9e
David Truong <truongdung0502@gmail.com>
2018-06-22 19:28:05 -0500

    update project

---
+++ DotnetCoreHelloWorldTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotnetCoreHelloWorldTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Student s = new Student();
+            s.LastName = "Truong";
+            s.FirstName = "David";
+            Console.WriteLine(s.FullName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotnetCoreHelloWorldTutorial/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCoreHelloWorldTutorial
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+    }
+}
