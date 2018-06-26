5d1cc7e9b5be3a8a6d12d6222980ba5408076075
David Truong <truongdung0502@gmail.com>
2018-06-26 10:58:09 -0500

    update student class

--- DotnetCoreHelloWorldTutorial/Program.cs
+++ DotnetCoreHelloWorldTutorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DotnetCoreHelloWorldTutorial
 {
@@ -12,7 +13,36 @@ namespace DotnetCoreHelloWorldTutorial
             s.LastName = "Truong";
             s.FirstName = "David";
             Console.WriteLine(s.FullName);
+
+            Func<string, string> convert = q => q.ToUpper();
+            string[] words = { "orange", "apple", "Article", "elephant" };
+            var words1 = words.Select(convert);
+
+            foreach (var item in words1)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadLine();
+        }
+
+        public static Boolean isPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            int currentNumber = x;
+            int newNumber = 0;
+
+            while (x != 0)
+            {
+                int nextNo = newNumber * 10 + x % 10;
+                newNumber = nextNo;
+                x = x / 10;
+            }
+
+            if (currentNumber != newNumber)
+                return false;
+            return true;
         }
     }
 }
